a5e30fed1ec80f8b1f7837c79119c52a1c72800c
Dendi Santoso <dendi.santoso74@gmail.com>
2023-07-31 09:53:45 +0700

    init commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import "../../app/globals.css";
+import style from "./home.module.scss";
+
+const Home = () => {
+  return (
+    <div className={style.home}>
+      <div className='w-1/2'>
+        <h1>Hi, I’m xxxx 👋</h1>
+        <p className='text-justify'>
+          that are fast, accessible, visually appealing, and responsive. Even
+          though I have been creating web applications for over 7 years, I still
+          love it as if it was something new.
+        </p>
+      </div>
+      <div className='w-1/2'>
+        <Image
+          src='/images/pic.png'
+          alt='profile photo'
+          width={100}
+          height={100}
+        />
+      </div>
+
+      <div className='border-2 border-yellow-700'>xxx</div>
+    </div>
+  );
+};
+
+export default Home;
